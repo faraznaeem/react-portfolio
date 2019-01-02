2abcee1d8eb3ec4719168eb90dd87e8e57166eb8
faraznaeem <faraznaeem@gmail.com>
2019-01-02 13:37:07 +0100

    Adds routing to about and projects components

--- src/header.jsx
+++ src/header.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className="flex items-center justify-between flex-wrap bg-blue-darkest p-6">
-            <h1 className="text-white text-3xl">My Portfolio</h1>
-        </nav>
+        <nav className="header">
+        <h1 className="header-title">
+            <Link className="text-white hover:text-grey no-underline" to='/'>My Portfolio</Link>
+        </h1>
+        <ul className="list-reset flex">
+            <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/about'>About Me</NavLink></li>
+            <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/projects'>My Projects</NavLink></li>
+        </ul>
+    </nav>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,20 +4,32 @@ import Hello from "./Hello"
 import Header from "./Header"
 import Footer from "./Footer"
 import './css/tailwind.css'
+import { BrowserRouter } from 'react-router-dom'
+import Projects from "./Projects"
+import About from "./About"
+import { Switch, Route } from 'react-router-dom'
 
 
 const App = () => {
     return (
-        <div className="min-h-screen">
-        <div className="flex flex-col w-full fixed pin-l pin-y">
-            <Header />
-            <div className="px-4 pt-4 flex-1 overflow-y-scrol">
-                <Hello />
-            </div>
-            <Footer />
+        <div className="page-wrapper">
+            <div className="page-content">
+                <Header />
+                <div className="content-wrapper">
+                    <Switch>
+                        <Route exact path='/' component={Hello}></Route>
+                        <Route exact path='/about' component={About}></Route>
+                        <Route exact path='/projects' component={Projects}></Route>
+                    </Switch>
+                </div>
+                <Footer />
+            </div >
         </div >
-    </div >
     )
 };
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('app'));
